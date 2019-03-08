c9010290eb13e42b5df7f87770cc37dd4e75285d
Camry Chen <camry.chen@foxmail.com>
2019-03-08 16:30:27 +0800

    add WebBootstrap

---
+++ src/DoveFramework/Bootstrap/WebBootstrap.php
@@ -0,0 +1,13 @@
+<?php
+namespace DoveFramework\Bootstrap;
+
+/**
+ * 基于 Swoole 的 Web 服务启动器。
+ *
+ * @package       DoveFramework\Bootstrap
+ * @author        Camry Chen <camry.chen@foxmail.com>
+ * @version       1.0.0
+ * @copyright (c) 2018-2019, Camry Chen
+ */
+class WebBootstrap extends HTTPBootstrap {
+}
--- src/DoveFramework/Bootstrap/WebSocketBootstrap.php
+++ src/DoveFramework/Bootstrap/WebSocketBootstrap.php
@@ -9,7 +9,7 @@ namespace DoveFramework\Bootstrap;
  * @version       1.0.0
  * @copyright (c) 2018-2019, Camry Chen
  */
-class WebSocketBootstrap extends AbstractBootstrap {
+class WebSocketBootstrap extends AbstractSwooleBootstrap {
     /**
      * 初始化完成调用方法。
      */

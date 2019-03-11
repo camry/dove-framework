c52c7e9ac4890fe1ff7217b2797f0fd87542e26d
Camry Chen <camry.chen@foxmail.com>
2019-03-11 15:29:52 +0800

    update

--- src/DoveFramework/Context/DefaultContext.php
+++ src/DoveFramework/Context/DefaultContext.php
@@ -30,11 +30,11 @@ class DefaultContext extends AbstractBase implements IContext {
 
         // 数据库
         if ($this->bootstrap->cli)
-            $idle_timeout = 7776000;
+            $timeout = 7776000;
         else
-            $idle_timeout = 28800;
+            $timeout = 28800;
 
-        $this->dbo = new DbPdo($this->bootstrap, new DbParameter($cfgs['db']['host'], $cfgs['db']['port'], $cfgs['db']['user'], $cfgs['db']['pass'], $cfgs['db']['name'], $cfgs['db']['charset'], ( string ) $cfgs['db']['socket'], $idle_timeout));
+        $this->dbo = new DbPdo($this->bootstrap, new DbParameter($cfgs['db']['driver'], $cfgs['db']['host'], $cfgs['db']['port'], $cfgs['db']['user'], $cfgs['db']['pass'], $cfgs['db']['name'], $cfgs['db']['charset'], ( string ) $cfgs['db']['socket'], $timeout));
     }
 
     /**

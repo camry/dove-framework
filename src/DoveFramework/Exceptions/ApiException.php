d492f03131c73ccd8bdf007c606ae03e257b2fdc
Camry Chen <camry.chen@foxmail.com>
2019-04-20 12:53:51 +0800

    UPDATE

---
+++ src/DoveFramework/Exceptions/ApiException.php
@@ -0,0 +1,13 @@
+<?php
+namespace DoveFramework\Exceptions;
+
+/**
+ * Api 异常。
+ *
+ * @package       DoveFramework\Exceptions
+ * @author        Camry Chen <camry.chen@foxmail.com>
+ * @version       1.0.0
+ * @copyright (c) 2018-2019, Camry Chen
+ */
+class ApiException extends WebException {
+}
---
+++ src/DoveFramework/Exceptions/WebException.php
@@ -0,0 +1,13 @@
+<?php
+namespace DoveFramework\Exceptions;
+
+/**
+ * Web 异常。
+ *
+ * @package       DYGame\Exceptions
+ * @author        Camry Chen <camry.chen@foxmail.com>
+ * @version       1.0.0
+ * @copyright (c) 2018-2019, Camry Chen
+ */
+class WebException extends BaseException {
+}
